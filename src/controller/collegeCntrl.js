06bc78a1bd33d5b8ab66d311d137a9e5fa0fdd98
Ketan Gupta <guptaketan6375@gmail.com>
2023-06-09 12:59:03 +0530

    axios added

--- src/controller/collegeCntrl.js
+++ src/controller/collegeCntrl.js
@@ -1,5 +1,6 @@
 const collegeModel = require('../Models/collegeModel');
 const validator = require('validator');
+const axios = require('axios');
 
 
 
@@ -10,11 +11,17 @@ const createClg = async (req, res) => {
         if (!fullName || fullName.trim() == '') return res.status(400).json({ status: false, message: 'Full Name is required' });
         if (!logoLink || logoLink.trim() == '' || !validator.isURL(logoLink)) return res.status(400).json({ status: false, message: 'Logo Link is required' });
         else {
-            const findCollege = await collegeModel.findOne({ name: name });
-            if(findCollege) return res.status(400).json({ status: false, message: 'College already exists' });
-            const college = (await collegeModel.create(req.body));
-            const selectedData = {name: college.name,fullName: college.fullName, logoLink: college.logoLink , isDeleted: college.isDeleted};
-            res.status(201).json({ status: true, data: selectedData });
+            await axios.get(logoLink)
+                .then(async () => {
+                    const findCollege = await collegeModel.findOne({ name: name });
+                    if (findCollege) return res.status(400).json({ status: false, message: 'College already exists' });
+                    const college = (await collegeModel.create(req.body));
+                    const selectedData = { name: college.name, fullName: college.fullName, logoLink: college.logoLink, isDeleted: college.isDeleted };
+                    res.status(201).json({ status: true, data: selectedData });
+                })
+                .catch( error => {
+                    res.status(404).json({ status: false, message: error.message });
+                })
         }
     } catch (error) {
         res.status(500).json({ status: false, message: error.message });
@@ -22,4 +29,4 @@ const createClg = async (req, res) => {
 }
 
 
-module.exports = {createClg}
+module.exports = { createClg }
